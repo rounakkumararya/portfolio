b1d6ac48165cc8ff614b66b515f4f60bc221790c
Rounak <rounakkumar040@gmail.com>
2023-04-01 20:43:14 +0530

    added some functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // toggle icon anvbar
 
 let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('header nav a');
+let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
@@ -38,5 +38,23 @@ window.onscroll = () => {
 
     header.classList.toggle('sticky', window.scrollY > 100)
 
+    // remove toggle icon and navbar when click navbar link (scroll)
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
+
+/* Scroll Reveal*/
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-con, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-con h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-con p, .about-content', { origin: 'right' });
